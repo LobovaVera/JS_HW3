0aac3070dae4d0c23881dab5b81f618a58d293bc
Vera <pchilli@yandex.ru>
2024-01-15 14:35:30 +0300

    homework 3 done

--- 2.js
+++ 2.js
@@ -9,4 +9,21 @@
 сообщение "Размер заработной платы за вычетом налогов равен N."
 */
 
-// Здесь пишем решение, данный комментарий необходимо стереть.
+
+
+function salaryValidation(salary) {
+    if (Number.isNaN(Number(salary)) || salary.trim() === "" || salary < 0) {
+        return "Значение задано неверно";
+    } else {
+        salary = Number(salary);
+        return `Размер заработной платы за вычетом налогов равен ${getAfterTaxSalary(salary.toFixed(2))}`;
+    }
+}
+
+function getAfterTaxSalary(salary) {
+    let a = (salary * 0.87).toFixed(2);
+    return a;
+}
+const salaryInput = prompt("Введите размер своей заработной платы: ");
+console.log(salaryValidation(salaryInput));
+
--- 3.js
+++ 3.js
@@ -10,4 +10,43 @@
 три числа. Проверять их не нужно.
 */
 
-// Здесь пишем решение, данный комментарий необходимо стереть.
+/**
+ * Просит пользователя ввести числа и возвращает из массив.
+ * @param {number} howManyNumbers количесто чисел, которые вы хотите получить от пользователя.
+ * @return {array} numbers - массив чисел, введенных пользователем.
+ */
+function getNumbersFromUser(howManyNumbers) {
+    let numbers = [];
+    for (let i = 0; i < howManyNumbers; i++) {
+        numbers[i] = prompt(`Введите число ${i + 1}`);
+    }
+    return numbers;
+}
+
+
+
+/**
+ * Функция выводит через alert большее число массива
+ * @param {array} numbersArray - массив чисел
+ */
+function getMaxNumberHW(numbersArray = []) {
+    let max = Number(numbersArray[0]);
+    for (let i = 1; i <= numbersArray.length - 1; i++) {
+        if (max < numbersArray[i]) {
+            max = numbersArray[i];
+        }
+    }
+    alert(`Максимальное значение среди чисел ${numbersArray[0]}, ${numbersArray[1]}, ${numbersArray[2]} равно ${max}.`);
+}
+
+let inputNumbers = getNumbersFromUser(3);
+getMaxNumberHW(inputNumbers);
+
+
+//если нужно именно три числа, можно еще так
+
+// function getMaxNumberHW(num1, num2, num3) {
+//     let max = Math.max(+num1, +num2, +num3);
+//     alert(`Максимальное значение среди чисел ${num1}, ${num2}, ${num3} равно ${max}.`);
+// }
+// getMaxNumberHW(inputNumbers[0], inputNumbers[1], inputNumbers[2]);
